b77c397909801fda89e4b59653f1cda628e5184d
Justine <Justine@DESKTOP-85HMEMV>
2024-12-13 14:18:27 +0800

    g

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿
+IF NOT EXISTS (SELECT * FROM Users WHERE Username = 'Justine' AND IsAdmin = 1)
+BEGIN
+   
+    INSERT INTO Users (Username, Password, IsAdmin)
+    VALUES ('Justine', 'velskud', 1); 
+
+END
+   
+   /* IF NOT EXISTS (SELECT * FROM Users WHERE Username = 'AdminUser1' AND IsAdmin = 1)
+BEGIN
+    INSERT INTO Users (Username, Password, IsAdmin)
+    VALUES ('AdminUser1', 'adminpass1', 1);
+END
